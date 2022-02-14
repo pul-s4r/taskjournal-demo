81e85b735fc261b08ced3f056f3f734f33ae3377
Junji Moey <jjmoey@gmail.com>
2022-02-14 17:00:08 +1100

    Change server port, add CORS

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 import journalRoutes from './routes/journalRoutes.js'
 
 // const express = require('express');
 // const journalRoutes = require('./routes/journalRoutes.js');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // });
 
-app.use(express.json()); 
+app.use(express.json());
+app.use(cors());
 app.use('/', journalRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`); 
 });
