370ce8b30eb1ff130ca2aee3fce687224b81faae
Junji Moey <jjmoey@gmail.com>
2022-02-20 22:19:56 +1100

    Add indexed method to AccountActions

--- server/lib/accountActions.js
+++ server/lib/accountActions.js
@@ -1,15 +1,24 @@
+const maxIdx = 10;
+
 const AccountActions = {
   // rewrite to accept todo
   getAccount: async (web3) => {
+    return AccountActions.getAccountIndexed(web3, 0);
+  },
+  getAccountIndexed: async (web3, idx) => {
+    if (idx < 0 || idx >= maxIdx) {
+      console.warn('Account index exceeds maximum available, defaulting to 0'); 
+      idx = 0;
+    }
     return new Promise((resolve, reject) => {
       web3.eth.getAccounts((error, accounts) => {
         if(typeof error === null) {
           return reject(error);
         }
-        resolve(accounts[0]);
+        resolve(accounts[idx]);
       });
     });
-  },
+  }
 };
 
 export default AccountActions;
