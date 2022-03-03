e5f7a7e6ca5dc4d913b3101996a6b6e8d0fb657e
Junji Moey <jjmoey@gmail.com>
2022-03-03 19:20:27 +1100

    Add auth to server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,34 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import journalRoutes from './routes/journalRoutes.js'
 
-// const express = require('express');
-// const journalRoutes = require('./routes/journalRoutes.js');
+import passport from 'passport';
+import mongoose from 'mongoose';
+
+import { config } from './store/config.js';
+import { applyPassportStrategy } from './store/passport.js';
+import { userController } from './controller/index.js';
 
 const app = express();
 const port = 8000;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// });
-
 app.use(express.json());
 app.use(cors());
-app.use('/', journalRoutes);
 
+applyPassportStrategy(passport);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', journalRoutes);
+app.use('/auth', userController);
+
+const { mongoDBUri, mongoHostName } = config.env;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  mongoose
+    .connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log(`Conneted to mongoDB at ${mongoHostName}`);
+    });
 });
