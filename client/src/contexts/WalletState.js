04d712561a77e83d056f4d1475fff30d8cd3b308
Junji Moey <jjmoey@gmail.com>
2022-03-05 13:24:43 +1100

    Add wallet/provider state and reducers

---
+++ client/src/contexts/WalletState.js
@@ -0,0 +1,33 @@
+export const initialWalletState = {
+  provider: null,
+  web3Provider: null,
+  address: null,
+  chainId: null,
+};
+
+export function walletReducer(state, action) {
+  switch (action.type) {
+    case 'SET_WEB3_PROVIDER':
+      return {
+        ...state,
+        provider: action.provider,
+        web3Provider: action.web3Provider,
+        address: action.address,
+        chainId: action.chainId,
+      };
+    case 'SET_ADDRESS':
+      return {
+        ...state,
+        address: action.address,
+      };
+    case 'SET_CHAIN_ID':
+      return {
+        ...state,
+        chainId: action.chainId,
+      };
+    case 'RESET_WEB3_PROVIDER':
+      return initialWalletState;
+    default:
+      throw new Error();
+  };
+};
