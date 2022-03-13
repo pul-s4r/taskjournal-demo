e1b276921c467fb5a60df636003bb82409c8fbec
Junji Moey <jjmoey@gmail.com>
2022-03-14 02:03:37 +1100

    Expand contract instance schema

--- server/database/schemas/contractinst.schema.js
+++ server/database/schemas/contractinst.schema.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const contractinstSchema = new mongoose.Schema({
-  name: {},
-  id: {},
-  network: {},
-  address: {},
-  users: {},
+  name: { type: String, required: true },
+  contractdefId: { type: ObjectId, required: true },
+  networkId: { type: Number, required: true },
+  providerUrl: { type: String, required: true },
+  address: { type: String, required: true },
+  owner: { type: ObjectId, required: true },
+  users: { type: [ObjectId], required: false },
+  dateCreated: { type: Date, required: false },
 });
 
 export default contractinstSchema;
