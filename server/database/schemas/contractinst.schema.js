db41c16c64bfe29fcd9041d29c10b9f4751af25b
Junji Moey <jjmoey@gmail.com>
2022-03-14 15:41:26 +1100

    Add unique names to contract instance schema

--- server/database/schemas/contractinst.schema.js
+++ server/database/schemas/contractinst.schema.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const contractinstSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   contractdefId: { type: ObjectId, required: true },
   networkId: { type: Number, required: true },
   providerUrl: { type: String, required: true },
