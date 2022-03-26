3d21dd5d0d8197ee82c92e653cb7438baf5d16f9
Junji Moey <jjmoey@gmail.com>
2022-03-26 16:34:58 +1100

    Add new contracts API, route to get contract definitions

---
+++ client/src/api/contractAPI.js
@@ -0,0 +1,29 @@
+import Utils from './utils.js';
+
+const serverurl = 'http://localhost:8000';
+
+const ContractAPI = {
+  getContractDefs: () => {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${Utils.getToken()}`,
+      }
+    };
+    var result = fetch(`${serverurl}${"/contract/definition"}`, options)
+      .then((res) => {
+        console.log(res);
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(`Error in call to getTasks: ${res.json()}`);
+        }
+      })
+      .catch((error) => {
+        console.warn(`API_ERROR: ${error}`);
+      });
+    return result;
+  },
+};
+
+export default ContractAPI;
