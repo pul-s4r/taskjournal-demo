71e550c363c563373fa9ac3cef3e5118511f2804
Junji Moey <jjmoey@gmail.com>
2022-03-26 16:37:42 +1100

    Add date string fomratter to utils

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -17,4 +17,15 @@ const formatDate = (rawDate) => {
   }
 };
 
-export { formatDate };
+const formatDateString = (dateStr) => {
+  try {
+    const _date = new Date(dateStr);
+    const date = `${_date.getDate() + 1}/${_date.getMonth() + 1}/${_date.getFullYear()}`;
+    const time = `${_date.getHours()}:${_date.getMinutes()}`;
+    return `${date} - ${time}`;
+  } catch (error) {
+    return `${dateStr}`;
+  }
+};
+
+export { formatDate, formatDateString };
