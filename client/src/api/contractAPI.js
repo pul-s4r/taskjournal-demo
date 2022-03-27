9404015d0848941be52159b2caf857358dd0d147
Junji Moey <jjmoey@gmail.com>
2022-03-27 12:41:28 +1100

    Append files to formData instead of set

--- client/src/api/contractAPI.js
+++ client/src/api/contractAPI.js
@@ -25,7 +25,37 @@ const ContractAPI = {
     return result;
   },
   addContractDef: (name, type, files, main) => {
-    
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append("files", files[i]);
+    }
+    formData.set("info", JSON.stringify({
+      "name": name,
+      "type": type,
+      "link" : "",
+      "main": main
+    }));
+    const options = {
+      method: "POST",
+      headers: {
+        // "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${Utils.getToken()}`,
+      },
+      // mode: "no-cors",
+      body: formData,
+    };
+    var result = fetch(`${serverurl}${"/contract/definition/add"}`, options)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error(`Error in call to getTasks: ${res.json()}`);
+        }
+      })
+      .catch((error) => {
+        console.warn(`API_ERROR: ${error}`);
+      });
+    return result;
   }
 };
 
