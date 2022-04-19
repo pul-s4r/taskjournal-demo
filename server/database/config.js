44047fb37902ca7ee6a7cb3c4bed4428f6d94c62
Junji Moey <jjmoey@gmail.com>
2022-04-19 15:26:47 +1000

    Move mongodb password to env

--- server/database/config.js
+++ server/database/config.js
@@ -1,6 +1,8 @@
 export const config = {
   env: {
-    mongoDBUri: process.env.NODE_ENV === 'production' ? 'mongodb+srv://tdb_access:C2022Riu34.@cluster0.ptvac.mongodb.net/taskdata?retryWrites=true&w=majority' : 'mongodb://localhost/test',
+    mongoDBUri: process.env.NODE_ENV === 'production' ? `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0.ptvac.mongodb.net/taskdata?retryWrites=true&w=majority` : 'mongodb://localhost/test',
     mongoHostName: process.env.NODE_ENV === 'production' ? 'mongodbAtlas' : 'localhost',
   },
 };
+
+console.log(config.env.mongoDBUri);
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import journalRoutes from './routes/journalRoutes.js'
 
 import passport from 'passport';
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 // import { config } from './store/config.js';
 import { config as dbConfig } from './database/config.js';
@@ -36,11 +37,15 @@ app.use('/contract/instance', contractinstController);
 const { mongoDBUri, mongoHostName } = dbConfig.env;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-  mongoose
-    .connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log(`Connected to mongoDB at ${mongoHostName}`);
-    });
+  try {
+    mongoose
+      .connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+        console.log(`Connected to mongoDB at ${mongoHostName}`);
+      });
+  } catch (error) {
+    console.error("Could not connect to mongoDB, reason: ", error);
+  }
 });
 
 export default app;
