7edda667ad9d6f42558cc9060cf2e832eeeb3233
Junji Moey <jjmoey@gmail.com>
2022-04-19 15:45:13 +1000

    Move mongo uri into env

--- server/database/config.js
+++ server/database/config.js
@@ -1,8 +1,6 @@
 export const config = {
   env: {
-    mongoDBUri: process.env.NODE_ENV === 'production' ? `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0.ptvac.mongodb.net/taskdata?retryWrites=true&w=majority` : 'mongodb://localhost/test',
+    mongoDBUri: process.env.NODE_ENV === 'production' ? `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@${process.env.MONGODB_ATLAS_ENDPOINT}/${process.env.MONGODB_ATLAS_DBNAME}?retryWrites=true&w=majority` : 'mongodb://localhost/test',
     mongoHostName: process.env.NODE_ENV === 'production' ? 'mongodbAtlas' : 'localhost',
   },
 };
-
-console.log(config.env.mongoDBUri);
